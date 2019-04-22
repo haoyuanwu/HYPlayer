9a8bb88de0ecd61c234643e1fb4df6aedd50d24c
wuhaoyuan <qw761754627@163.com>
2019-04-22 15:07:47 +0800

    Initial Commit

---
+++ HYPlayer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HYPlayer
+//
+//  Created by 吴昊原 on 2019/4/22.
+//  Copyright © 2019 HYPlayer. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HYPlayer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HYPlayer
+//
+//  Created by 吴昊原 on 2019/4/22.
+//  Copyright © 2019 HYPlayer. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
